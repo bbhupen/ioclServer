3f08421d5b2825aace503f410dca1d7dbed1eaf9
bbhupen <bbhupen601@gmail.com>
2022-07-24 22:17:35 +0530

    modify app.js

--- app.js
+++ app.js
@@ -11,17 +11,18 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use(cors())
+app.use(cors({
+  origin: "https://ioclpushclient.herokuapp.com"
+}))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const publicVapidKey = process.env.VAPID_PUBLIC_KEY
-const privateVapidKey= process.env.VAPID_PRIVATE_KEY
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY
 const PORT = process.env.PORT || 5000
 const username = process.env.UNAME
 const password = process.env.PASSWORD
 
-app.use(cors({origin: 'https://ioclpushclient.herokuapp.com/'}));
 
 webPush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey)
 
@@ -30,9 +31,10 @@ const Sub = mongoose.model('Sub')
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.jwows.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(uri, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true })
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 mongoose.connection.on("error", function (error) {
   console.log(error)
@@ -43,15 +45,15 @@ mongoose.connection.on("open", function () {
 })
 
 
-app.post("/subscribe", async (req,res) => {
+app.post("/subscribe", async (req, res) => {
   const subscription = req.body;
   console.log(subscription)
 
 
-  const isSubbed = await Sub.findOne({subscription: subscription})
+  const isSubbed = await Sub.findOne({ subscription: subscription })
 
-  if (!isSubbed){
-    const newSub = new Sub({subscription: subscription})
+  if (!isSubbed) {
+    const newSub = new Sub({ subscription: subscription })
 
     await newSub.save()
     console.log("Saved in database")
@@ -63,28 +65,28 @@ app.post("/subscribe", async (req,res) => {
 })
 
 
-app.post('/sendNotification', async(req,res)=> {
-  const {title, body} = req.body
+app.post('/sendNotification', async (req, res) => {
+  const { title, body } = req.body
 
   const payload = JSON.stringify({
-    title: title, 
+    title: title,
     body: body,
   })
 
   console.log(payload)
 
   const sub = await Sub.find()
-  sub.map((obj,i)=> {
+  sub.map((obj, i) => {
     console.log(obj.subscription)
     webPush.sendNotification(obj.subscription, payload)
-    .catch(err => console.log(err))
+      .catch(err => console.log(err))
   })
 })
 
 
 
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log("Server listening on Port", PORT);
 })
 
